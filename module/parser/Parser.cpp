b7c3670d79485908a271ae4b74370a368f0ff0f1
zhi <1665052870@qq.com>
2018-12-08 14:17:15 +0800

    modified parser

--- module/parser/Parser.cpp
+++ module/parser/Parser.cpp
@@ -3,9 +3,11 @@
 Parser parser;
 
 bool Parser::parse() {
-    tk = scanner.next();
+    int subProgram;
+
     if(tk.getCategory() == Token::id) {
-        //TODO 记录子程序id
+        //TODO 生成子程序开头四元式
+        subProgram = tk.getOffset();
         tk = scanner.next();
         sentence();
     } else {
@@ -14,10 +16,23 @@ bool Parser::parse() {
     }
 
     if(tk.getCategory() != Token::symbol 
-            || opList.get(tk.getOffset()) == ";") {
+            || opList.get(tk.getOffset()) != ";") {
         //没有以分号结尾,返回错误1
         return false;
     }
+    
+    tk = scanner.next();
+    if(tk.getCategory != Token::keyword
+            || keywordList.get(tk.getOffset()) != "end") {
+        //TODO 回到sentence();
+    }
+
+    tk = scanner.next();
+    if(tk.getCategory != Token::id
+            || subProgram != tk.getOffset()) {
+        return false;
+    }
+    //TODO 合法句子,生成结尾四元式
     return true;
 }
 
@@ -27,4 +42,8 @@ void Parser::sentence() {
 
 void Parser::expression() {
     
+}
+
+void Parser::content() {
+
 }
--- module/parser/Parser.h
+++ module/parser/Parser.h
@@ -7,11 +7,15 @@
 
 class Parser {
 public:
+    Parser() { tk = scanner.next(); }
     bool parse();//分析一个句子,返回是否合法并将合法句子生成四元式
+
 private:
     Token tk;
+    bool ok;
     void sentence();
     void expression();
+    void content();
 };
 
 extern Parser parser;
